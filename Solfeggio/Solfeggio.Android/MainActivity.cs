53736b5424309cbf723d60252f48f1bbc098884a
makeman <makeman@tut.by>
2018-08-30 14:27:24 +0300

    MAKE !

--- Solfeggio/Solfeggio.Android/MainActivity.cs
+++ Solfeggio/Solfeggio.Android/MainActivity.cs
@@ -1,34 +1,56 @@
 ﻿using System;
-
+using System.Linq;
+using Android;
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Ace;
+using Android.Support.V4.App;
+using Android.Widget;
 
 namespace Solfeggio.Droid
 {
-    [Activity(Label = "Solfeggio", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    [Activity(
+        Label = "Solfeggio",
+        Icon = "@mipmap/icon",
+        Theme = "@style/MainTheme",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        private static readonly string[] RequriedPermissions = {Manifest.Permission.RecordAudio};
+
         protected override void OnCreate(Bundle bundle)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
-
-            base.OnCreate(bundle);
-
             try
             {
-                global::Xamarin.Forms.Forms.Init(this, bundle);
+                TabLayoutResource = Resource.Layout.Tabbar;
+                ToolbarResource = Resource.Layout.Toolbar;
+
+                base.OnCreate(bundle);
+
+                Xamarin.Forms.Forms.Init(this, bundle);
                 LoadApplication(new App());
+
+                if (RequriedPermissions.Any(p => CheckSelfPermission(p).IsNot(Permission.Granted)))
+                {
+                    ActivityCompat.RequestPermissions(this, RequriedPermissions, RequestPermissionCode);
+                }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
             }
         }
+
+        private const int RequestPermissionCode = 1000;
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            for (var i = 0; i < grantResults.Length; i++)
+            {
+                Toast.MakeText(this, $"{permissions[i]} : {grantResults[i]}", ToastLength.Short);
+            }
+        }
     }
 }
-
