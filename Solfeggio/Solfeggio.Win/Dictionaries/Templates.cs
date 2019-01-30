70c628f5707dceb5a75593e67107ddc3e5a0d25e
makeman <makeman@tut.by>
2019-01-31 01:16:30 +0300

    MAKE !

---
+++ Solfeggio/Solfeggio.Win/Dictionaries/Templates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solfeggio.Dictionaries
+{
+    public partial class Templates
+    {
+        public Templates()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Solfeggio/Solfeggio/Converters/PowConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Solfeggio.Converters
+{
+	public class PowConverter : IValueConverter
+	{
+		public PowConverter()
+		{
+			Base = Math.E;
+		}
+
+		public double Base { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            Math.Log((double)value, Base);
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            Math.Pow(Base, (double)value); //Math.Exp((double) value);
+    }
+}
