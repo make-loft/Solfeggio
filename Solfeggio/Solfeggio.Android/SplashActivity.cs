cc95012c9d564a9d65a66d29301853a615b291f3
Makeloft <makeman@tut.by>
2023-01-29 23:57:11 +0300

    MAKE !

--- Solfeggio/Solfeggio.Android/SplashActivity.cs
+++ Solfeggio/Solfeggio.Android/SplashActivity.cs
@@ -5,11 +5,16 @@ using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
+using Android.Util;
 using Android.Views;
 
 using System;
+using System.IO;
+using System.Threading;
 
 using Xamarin.Essentials;
+
+using Yandex.Metrica;
 
 namespace Solfeggio.Droid
 {
@@ -36,8 +41,8 @@ namespace Solfeggio.Droid
 			AppDomain.CurrentDomain.UnhandledException += (o, args) => ProcessUnhandledException(args.ExceptionObject as Exception);
 			async void ProcessUnhandledException(Exception exception)
 			{
-				//for (var e = exception; e.Is(); e = e.InnerException)
-				//	YandexMetrica.ReportUnhandledException(e);
+				for (var e = exception; e.Is(); e = e.InnerException)
+					YandexMetrica.ReportUnhandledException(e);
 
 				await this.ShowAlertDialogAsync
 				(
@@ -51,6 +56,8 @@ namespace Solfeggio.Droid
 			Window.SetFlags(WindowManagerFlags.Fullscreen, WindowManagerFlags.Fullscreen);
 
 			base.OnCreate(bundle);
+
+			ThreadPool.QueueUserWorkItem(RunAnalitics);
 
 			Platform.Init(this, bundle);
 			Xamarin.Forms.Forms.Init(this, bundle);
@@ -80,6 +87,28 @@ namespace Solfeggio.Droid
 			);
 
 			FinishAffinity();
+		}
+
+		private void RunAnalitics(object o)
+		{
+			var (androidId, advertisingId) = default(string);
+
+			try { androidId = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId); }
+			catch { }
+			//try { advertisingId = AdvertisingIdClient.GetAdvertisingIdInfo(ApplicationContext).Id; }
+			//catch { }
+			try
+			{
+				var appDataFolderPath = System.Environment.SpecialFolder.ApplicationData.GetPath();
+				var metricaFolderPath = Path.Combine(appDataFolderPath, "Yandex.Metrica");
+
+				YandexMetricaFolder.SetCurrent(metricaFolderPath);
+				YandexMetrica.Activate(App.YandexMetricaKeys[App.Edition], androidId, advertisingId);
+			}
+			catch (Exception exception)
+			{
+				Log.Error("Yandex.Metrica", exception.ToString());
+			}
 		}
 	}
 }
--- Solfeggio/Solfeggio/App.xaml.cs
+++ Solfeggio/Solfeggio/App.xaml.cs
@@ -4,16 +4,34 @@ using Solfeggio.Presenters;
 using Solfeggio.ViewModels;
 
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 
 using Xamarin.Forms.Xaml;
+
+using Yandex.Metrica;
+
+using static Solfeggio.Editions;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Skip)]
 namespace Solfeggio
 {
 	public partial class App
 	{
+#if DEBUG
+		public static Editions Edition { get; } = Developer;
+#else
+		public static Editions Edition { get; } = Education;
+#endif
+		public static Dictionary<Editions, string> YandexMetricaKeys = new()
+		{
+			{ Developer, "4722c611-c016-4e44-943b-05f9c56968d6" },
+			{ Portable, "d1e987f6-1930-473c-8f45-78cd96bb5fc0" },
+			{ Education, "37e2b088-6a25-4987-992c-e12b7e020e85" },
+			{ Gratitude, "e23fdc0c-166e-4885-9f18-47cc7b60f867" },
+		};
+
 		public static readonly string AppDataFolderPath =
 			Environment.SpecialFolder.ApplicationData.GetPath();
 
@@ -33,16 +51,23 @@ namespace Solfeggio
 			MainPage = new Views.SolfeggioView();
 		}
 
-		protected override void OnStart() { }
+		protected override void OnStart()
+		{
+			YandexMetrica.Wake();
+		}
 		protected override void OnSleep()
 		{
 			Store.Get<ProcessingManager>().ActiveProfile?.Dispose();
 
 			Store.Snapshot();
+
+			YandexMetrica.Lull();
 		}
 
 		protected override void OnResume()
 		{
+			YandexMetrica.Wake();
+
 			Store.Get<ProcessingManager>().ActiveProfile?.Expose();
 		}
 	}
