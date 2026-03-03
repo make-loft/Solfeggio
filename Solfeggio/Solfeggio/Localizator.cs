b7fbe99e597d539c91e6fd99339915ce316cb0b7
MAKEBOOK\Makeman <makeman@tut.by>
2026-03-03 15:44:39 +0300

    MAKE !

--- Solfeggio/Solfeggio/Localizator.cs
+++ Solfeggio/Solfeggio/Localizator.cs
@@ -165,6 +165,7 @@ namespace Solfeggio
 		public static o Bemole = _("Bemole", "Бемоль");
 		public static o Combined = _("Combined", "Комбинировано");
 		public static o Range = _("Range", "Диапазон");
+		public static o Error = _("Error", "Ошибка");
 		public static o Oops = _("Oops", "Упс");
 		public static o Ok = _("Ok", "Ок");
 
